8ced3e83503e0febe712ba944350d580ab722cb8
wermos <63574588+wermos@users.noreply.github.com>
2025-06-24 13:14:13 +0530

    Added two working examples.

---
+++ examples/basic_example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <beman/indices_view/indices.hpp>
+
+namespace biv = beman::indices_view;
+
+// Example usage of indices_view to print indices from 0 to n-1; very basic example.
+int main() {
+    for (auto i : biv::indices(5)) {
+        std::cout << i; // prints 01234 
+    }
+    return 0;
+}
---
+++ examples/paper_example.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+#include <beman/indices_view/indices.hpp>
+
+namespace biv = beman::indices_view;
+
+// Example given in the paper for `std::views::indices`, adapted to work with C++20.
+int main() {
+    std::vector rng(5, 0);
+
+    std::cout << "[";
+    bool first = true;
+    for (auto i : biv::indices(std::ranges::size(rng))) {
+        if (!first) std::cout << ", ";
+        std::cout << i;
+        first = false;
+    }
+    std::cout << "]\n";
+
+    return 0;
+}
